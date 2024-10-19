879de2d9e374ff623b39a7053b56f3197ccba23a
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2024-10-19 10:50:30 -0500

    version 1.0

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('[data-collapse-toggle="navbar-default"]');
+    const navbarMenu = document.getElementById('navbar-default');
+  
+    toggleButton.addEventListener('click', function () {
+      navbarMenu.classList.toggle('hidden');
+    });
+  });
+  
