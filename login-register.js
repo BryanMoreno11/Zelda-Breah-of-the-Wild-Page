0178be8da1df742ba91f7d96468cf6b5f66529f2
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2024-10-26 07:59:50 -0500

    iniciar y cerrar sesion

--- login-register.js
+++ login-register.js
@@ -3,7 +3,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import {
   getAuth,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCOfqLPDoHCP1E6ShROX7YYteBmn3NYB6M",
   authDomain: "zelda-breath-of-the-wild-page.firebaseapp.com",
@@ -18,7 +20,7 @@ const auth = getAuth(app);
 //region funciones
 
 registro.addEventListener("click", register);
-login.addEventListener("click", login);
+login.addEventListener("click", loginFunction);
 
 async function register() {
   let email = document.getElementById("email").value; 
@@ -29,22 +31,32 @@ async function register() {
       email,
       password
     );
-    console.log("Usuario registrado:", userCredential.user);
+   mostrarMensaje("Registro exitoso", "¡Bienvenido!", "success");
   } catch (error) {
-    console.error("Error en el registro:", error.message);
+    mostrarMensaje("Error en el registro", error.message, "error");
   }
 }
 
-async function login() {
+async function loginFunction() {
   let email = document.getElementById("email").value; 
   let password = document.getElementById("password").value; 
   try {
-    const userCredential = await auth.signInWithEmailAndPassword(
+    const userCredential = await signInWithEmailAndPassword(auth,
       email,
       password
     );
+    mostrarMensaje("Inicio de sesión exitoso", "¡Bienvenido!", "success");
     console.log("Usuario conectado:", userCredential.user);
   } catch (error) {
+    mostrarMensaje("Error en el inicio de sesión", error.message, "error");
     console.error("Error en el inicio de sesión:", error.message);
   }
+}
+
+function mostrarMensaje(titulo, mensaje, icono) {
+  Swal.fire({
+    title: titulo,
+    text: mensaje,
+    icon: icono,
+  });
 }
--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+
+// Importar las funciones necesarias de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, onAuthStateChanged,signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOfqLPDoHCP1E6ShROX7YYteBmn3NYB6M",
+  authDomain: "zelda-breath-of-the-wild-page.firebaseapp.com",
+  projectId: "zelda-breath-of-the-wild-page",
+  storageBucket: "zelda-breath-of-the-wild-page.appspot.com",
+  messagingSenderId: "953911522581",
+  appId: "1:953911522581:web:8d8ca0eb364d3febdd251c",
+};
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('[data-collapse-toggle="navbar-default"]');
     const navbarMenu = document.getElementById('navbar-default');
@@ -6,4 +24,36 @@ document.addEventListener('DOMContentLoaded', function () {
       navbarMenu.classList.toggle('hidden');
     });
   });
-  
+
+
+  cerrar.addEventListener('click', function () {
+    signOut(auth).then(() => {
+      mostrarMensaje("Sesión cerrada", "¡Hasta luego!", "success");
+    }).catch((error) => {
+      mostrarMensaje("Error al cerrar sesión", error.message, "error");
+    });
+  });
+
+onAuthStateChanged(auth, (user) => {
+  let sesion = document.getElementById("sesion");
+  let cerrar = document.getElementById("cerrar");
+  if (user) {
+   sesion.classList.add("hidden");
+   cerrar.classList.remove("hidden");
+  } else {
+    sesion.classList.remove("hidden");
+    cerrar.classList.add("hidden");
+
+  }
+});
+
+
+
+function mostrarMensaje(titulo, mensaje, icono) {
+  Swal.fire({
+    title: titulo,
+    text: mensaje,
+    icon: icono,
+  });
+}
+
